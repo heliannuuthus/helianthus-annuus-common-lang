1f5838dc19b2cb88c73b47e2875db2c88868e170
heliannuuthus <heliannuuthus@gmail.com>
2024-03-09 14:07:36 +0800

    first commit

---
+++ helianthus-annuus-openapi/src/main/java/io/ghcr/heliannuuthus/OpenapiConfiguration.java
@@ -0,0 +1,53 @@
+package io.ghcr.heliannuuthus;
+
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Slf4j
+@Configuration
+public class OpenapiConfiguration {
+
+    @Value("${spring.application.name}")
+    private String application;
+
+    @Value("${server.port}")
+    private String port;
+
+    @Value("${server.address:localhost}")
+    private String serverAddress;
+
+    private License license() {
+        return new License()
+                .name("MIT")
+                .url("https://opensource.org/licenses/MIT");
+    }
+
+    private Info info() {
+        return new Info()
+                .title(application)
+                .version("v1.0.0")
+                .license(license());
+    }
+
+    private ExternalDocumentation externalDocumentation() {
+        return new ExternalDocumentation()
+                .description("heliannuuthus")
+                .url("https://heliannuuthus.github.io/");
+    }
+
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        log.info("openapi: http://{}:{}/redoc.html", serverAddress, port);
+        return new OpenAPI()
+                .info(info())
+                .externalDocs(externalDocumentation());
+    }
+}
