e34b55538392c07578644c77072d3f6a0454eb32
heliannuuthus <heliannuuthus@gmail.com>
2024-03-10 21:56:16 +0800

    ✨ Apollo meta service initializer

--- helianthus-annuus-apollo-client/src/main/java/io/ghcr/heliannuuthus/DevtoolsApplication.java
+++ helianthus-annuus-apollo-client/src/main/java/io/ghcr/heliannuuthus/DevtoolsApplication.java
@@ -10,6 +10,7 @@ public class DevtoolsApplication {
     public static void main(String[] args) {
         // https://docs.spring.io/spring-boot/docs/2.1.5.RELEASE/reference/html/using-boot-devtools.html#using-boot-devtools-restart-disable
         System.setProperty("spring.devtools.restart.enabled", "false");
+        System.setProperty("spring.profiles.active", "dev");
         SpringApplication.run(DevtoolsApplication.class, args);
     }
 }
